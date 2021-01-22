9aa2db8d1d7f529fcd514417d5574aef95947dbd
jack911jie <jack911jie@hotmail.com>
2021-01-22 16:36:41 +0800

    edit 05

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),'modules'))
+import days_cal
 import json
 import pandas as pd
 import numpy as np
@@ -9,7 +12,7 @@ from PIL import Image
 class MingHu:
     def __init__(self):
         self.dir=os.path.dirname(os.path.abspath(__file__))
-        with open (os.path.join(self.dir,'config'),'r',encoding='utf-8') as f:
+        with open (os.path.join(self.dir,'config.dazhi'),'r',encoding='utf-8') as f:
             lines=f.readlines()
         _line=''
         for line in lines:
@@ -20,59 +23,109 @@ class MingHu:
         self.cus_file_dir=config['会员档案文件夹']
 
     def read_cus(self,cus='MH001韦美霜',start_time='20150101',end_time=''):
+        #------------基本情况--------
+        out=Vividict()       
+
         if end_time=='':
             end_time=datetime.now()
         else:
             end_time=datetime.strptime('-'.join([end_time[0:4],end_time[4:6],end_time[6:]]),'%Y-%m-%d')
         start_time=datetime.strptime('-'.join([start_time[0:4],start_time[4:6],start_time[6:]]),'%Y-%m-%d')
-        interval=end_time-start_time
-        interval_days=interval.days        
-        nat = np.datetime64('NaT')
         xls_name=os.path.join(self.cus_file_dir,cus+'.xlsx')
+        
+        df_basic=pd.read_excel(xls_name,sheet_name='基本情况')        
+        out['nickname']=df_basic['昵称'].tolist()[0] #昵称
+        out['sex']=df_basic['性别'].tolist()[0] #性别
+
+        #年龄
+        if not df_basic['出生年月'].empty:
+            birth=df_basic['出生年月'].tolist()[0]
+            if len(str(birth))==4:
+                birth=str(birth)+'0101'
+                age=days_cal.calculate_age(birth)
+                out['age']=age
+            elif len(str(birth))==6:
+                birth=str(birth)+'01'
+                age=days_cal.calculate_age(birth)
+                out['age']=age
+            elif len(str(birth))==8:
+                age=days_cal.calculate_age(str(birth))
+                out['age']=age
+            else:
+                out['age']=''            
+        else:
+            out['age']='' 
+        
+
+        #------------身体数据--------
+        df_body=pd.read_excel(xls_name,sheet_name='身体数据')
+        df_body=df_body[(df_body['时间']>=start_time) & (df_body['时间']<=end_time)] #根据时间段筛选记录
+        df_body=df_body.fillna(0)
+        if df_body.empty:
+            out['body']=''
+        else:
+            body_recent=df_body[df_body['时间']==df_body['时间'].max()].values.tolist()[0]       
+            out['body']['time']=body_recent[0]
+            out['body']['ht']=body_recent[1]
+            out['body']['wt']=body_recent[2]
+            out['body']['bfr']=body_recent[3]
+            out['body']['chest']=body_recent[4]
+            out['body']['l_arm']=body_recent[5]
+            out['body']['r_arm']=body_recent[6]
+            out['body']['waist']=body_recent[7]
+            out['body']['hip']=body_recent[8]
+            out['body']['l_leg']=body_recent[9]
+            out['body']['r_leg']=body_recent[10]
+            out['body']['l_calf']=body_recent[11]
+            out['body']['r_calf']=body_recent[12]
+            
+
+        #------------训练数据--------
         infos=pd.read_excel(xls_name,sheet_name='训练情况',skiprows=2,header=None)
         infos=infos.iloc[:,0:10] #取前10列
         infos.columns=['时间','形式','目标肌群','有氧项目','有氧时长','力量内容','重量','次数','教练姓名','教练评语']
-        # infos.rename(columns={'0':'时间','Unnamed: 1':'形式','Unnamed: 2':'目标肌群', \
-        #                       'Unnamed: 3':'有氧项目','Unnamed: 4':'有氧时长','Unnamed: 5':'力量内容', \
-        #                           'Unnamed: 6':'重量','Unnamed: 7':'次数','Unnamed: 8':'教练姓名','Unnamed: 9':'教练评语',},inplace=True)
 
         infos=infos[(infos['时间']>=start_time) & (infos['时间']<=end_time)] #根据时间段筛选记录
-        # print(infos)
 
-        traing_times=infos['时间'].nunique()
-        
-        # print('会员训练次数：',traing_times)
+        #起止日期
+        out['interval']=[infos['时间'].min(),infos['时间'].max()]  
 
+        #抗阻训练
         train_dates=infos.groupby(['时间','目标肌群'])
-        # print(list(train_dates))
         train_big_type=[]
         for dt,itm in train_dates:
             train_big_type.append(list(dt))
-        # print(train_big_type)
-        df_train_big_type=pd.DataFrame(train_big_type)
-        df_train_big_type.columns=['时间','目标肌群']
-        # print(df_train_big_type)
-        _sum_train_items=df_train_big_type.groupby(['目标肌群'],as_index=False)
-        print(_sum_train_items.count())
-        sum_train_items=[]
-        # for dst_muscle,itm in _sum_train_items:
-        #     print(dst_muscle)
-        # print(sum_train_items)
-            
-        
-        # print(list(train_dates))
-        # k=datetime.strptime(str(train_dates[1]).split('T')[0],'%Y-%m-%d')  
-        # now=datetime.now()
-        # interval=now-k
+        df_train_big_type=pd.DataFrame(train_big_type)    
+        if not df_train_big_type.empty:   
+            df_train_big_type.columns=['时间','目标肌群']
+            _sum_train_items=df_train_big_type.groupby(['目标肌群'],as_index=False)
+            sum_train_items=pd.DataFrame(_sum_train_items.count())  
+            sum_train_items.dropna(axis=0, how='any', inplace=True)
+            sum_train_items=sum_train_items[sum_train_items['目标肌群']!=' '].values
+            if len(sum_train_items)>0:
+                for itm in sum_train_items:
+                    out['train']['muscle'][itm[0]]=itm[1]
+            else:
+                out['train']['muscle']=''
+        else:
+            out['train']['muscle']=''
 
-        # print(interval.days)
-        # print(t[0])
+        #有氧训练总时长
+        out['train']['oxy_time']=infos['有氧时长'].sum()
 
-        # fenzhu_times=infos.groupby(['时间','目标肌群'])
-        # print(fenzhu_times)
+        # print(out)
+        return out
 
+    def draw(self,cus='MH001韦美霜',start_time='20150101',end_time=''):
+        infos=self.read_cus(cus=cus,start_time=start_time,end_time=end_time)
+        print(infos)
+
+class Vividict(dict):
+    def __missing__(self, key):
+        value = self[key] = type(self)()
+        return value
 
 if __name__=='__main__':
     p=MingHu()
-    p.read_cus()
+    p.draw(cus='MH001韦美霜',start_time='20200901',end_time='20200905')
 
