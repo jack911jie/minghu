3bf44e450b56a76739e1e96e1d54e762d3115da2
jack911jie <jack911jie@hotmail.com>
2021-03-18 16:20:06 +0800

    增加将方形图片改为圆形函数 def round_corner(img,method='in')

--- modules/pic_transfer.py
+++ modules/pic_transfer.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.font_manager as fm
 from matplotlib.backends.backend_agg import FigureCanvasAgg
-from PIL import Image
+from PIL import Image,ImageDraw
 
 def mat_to_pil_img(fig):
     # 将plt转化为numpy数据
@@ -29,3 +29,49 @@ def mat_to_pil_img(fig):
     # # 转换为rgb图像
     # rgb_image = image[:, :, :3]
     return image
+
+def round_corner(img,method='in'):
+    if img.mode!='RGBA':
+        img=img.convert('RGBA')
+
+    if method=='out':
+        w,h=img.size
+        r=int(np.sqrt(w*w/4+h*h/4))
+        d=int(r*2)
+        bg=Image.new('RGBA',(d,d),'#FFFFFF')
+        scale=3
+        alpha_layer = Image.new('L', (d*scale, d*scale), 0)
+        draw=ImageDraw.Draw(alpha_layer)
+        draw.ellipse((0,0,d*scale,d*scale),fill='#FFFFFF')
+        alpha_layer=alpha_layer.resize((d,d))
+        
+        # bg.paste(alpha_layer,(0,0),mask=alpha_layer)
+        bg.paste(img,((d-w)//2,(d-h)//2),mask=img)
+        bg.putalpha(alpha_layer)
+        # bg.save('e:/temp/dq.png')
+    elif method=='in':        
+        w,h=img.size
+        d=min(w,h)
+        scale=3
+        alpha_layer=Image.new('L',(w*scale,h*scale),0)
+        draw=ImageDraw.Draw(alpha_layer)
+        if h>=w:
+            draw.ellipse((0,(h*scale-d*scale)//2,w*scale,(h*scale-d*scale)//2+d*scale),fill='#FFFFFF')
+            alpha_layer=alpha_layer.resize((w,h))
+            img.putalpha(alpha_layer)
+        else:
+            draw.ellipse(((w*scale-d*scale)//2,0,(w*scale-d*scale)//2+d*scale,h*scale),fill='#FFFFFF')
+            alpha_layer=alpha_layer.resize((w,h))
+            img.putalpha(alpha_layer)
+        bg=img
+
+        # alpha_layer.show()
+        # img.show()
+        # bg.show()
+    return bg
+
+if __name__=='__main__':
+    # img=Image.open('E:\\健身项目\\素材\\男性头像01.jpg')
+    img=Image.open('E:/temp/快快12月.jpg')
+    imgg=round_corner(img,method='in')
+    imgg.show()
