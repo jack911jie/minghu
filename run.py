dcb95f6b1cbf57388cf6b28658dca17486e44f2c
jack_at_mingcuigu <jack911jie@qq.com>
2021-08-30 18:13:23 +0800

    增加seven_GUI的入口

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__),'modules'))
 import main
+import seven_GUI
 
 # print(os.path.join(os.path.dirname(__file__),'modules'))
 
@@ -22,19 +23,27 @@ def auto_xls(cus_name_input='',mode='prgrm',gui=''):
     new_cus_fn=p.auto_cus_xls(cus_name_input=cus_name_input,mode=mode,gui=gui)
     return new_cus_fn
 
+def run_seven_gui(place='seven'):
+    minghu_gui=seven_GUI.GUI(place=place)
+    minghu_gui.creat_gui()
+    # minghu_gui.get_cus_list()
+
 
 if __name__=='__main__':
     #反馈
     # cus_feedback(cus='MH017李俊娴',ins='MHINS001陆伟杰',start_time='20210526',end_time='20210701')
 
     #批量录入
-    group_input()
+    # group_input()
 
     #当天课后生成
     # today_feedback(cus='MH024刘婵桢',ins='MHINS002韦越棋',date_input='20210619')
 
     #新增会员
     # auto_xls(cus_name_input='测试',mode='prgrm',gui='')
+
+    #运行最新版本的gui
+    run_seven_gui(place='minghu')
 
 
 
