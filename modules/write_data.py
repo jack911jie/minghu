fcefa8e924b597ec0da944abd50bb6289b49ea28
jack@new_mi <jack911jie@qq.com>
2023-06-22 17:35:51 +0800

    feat 批量处理xlsx数据至xlsm

--- modules/write_data.py
+++ modules/write_data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import openpyxl
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import NamedStyle
+from openpyxl.styles import numbers
 
 class WriteData:
     def write_to_xlsx(self,input_dataframe,output_xlsx,sheet_name,parse_date_col_name='时间'):
@@ -17,8 +20,11 @@ class WriteData:
             writer.sheets=dict((ws.title,ws) for ws in book.worksheets)         
             old_rows=old.shape[0]
             input_dataframe.to_excel(writer,sheet_name=sheet_name,startrow=old_rows+1,index=False,header=False)
+            
             writer.save()
             log_txt=str(input_dataframe.shape[0])+' 条数据追加完成，行号：'+str(old_rows+2)+'-'+str(old_rows+input_dataframe.shape[0]+1)
+
+            # self.convert_column_to_date_format(file_path=output_xlsx,sheet_name=sheet_name,column_name=parse_date_col_name)
 
             return log_txt
         
@@ -29,3 +35,31 @@ class WriteData:
         # print(df_diff)
         
         return df_diff
+
+    def convert_column_to_date_format(self,file_path,sheet_name, column_name):
+        wb = openpyxl.load_workbook(file_path,keep_vba=True)
+        sheet = wb[sheet_name]
+
+        # 获取列索引
+        column_index = None
+        for cell in sheet[1]:
+            if cell.value == column_name:
+                column_index = cell.column_letter
+                break
+
+        if column_index is not None:
+            # 设置日期格式
+            # date_style = NamedStyle(name='date_format')
+            # date_style.number_format = numbers.FORMAT_DATE_XLSX14
+            sheet.column_dimensions[column_index].width = 13  # 设置列宽
+            for cell in sheet[column_index]:
+                cell.number_format='yyyy/mm/dd'
+            wb.save(file_path)
+        wb.close()
+
+        print('{} 修改为短日期格式完成'.format(column_name))
+
+        
+if __name__=='__main__':
+    p=WriteData()
+    p.convert_column_to_date_format(file_path='E:\\temp\\minghu\\xlsm\\MH016徐颖丽.xlsm',sheet_name='购课表',column_name='收款日期')
--- tools/XlsxToXlsm.py
+++ tools/XlsxToXlsm.py
@@ -61,19 +61,87 @@ class XlsxToXlsm:
     def write_xlsm(self,df,out_name,sheet_name,parse_date_col_name=''):
         # fn='E:\\temp\\minghu\\01-会员管理\\会员资料\\MH120肖婕.xlsx'
         pass
-        
+    
 
+class CopyRecs:
+    def __init__(self,buy_fn,taken_fn):
+        self.df_buys=self.read_buys(buy_fn=buy_fn)
+        self.df_taken=self.read_takens(taken_fn=taken_fn)
+
+    def read_buys(self,buy_fn):
+        df_buy=pd.read_excel(buy_fn,sheet_name='购课财务流水')
+        df_buy['会员编码及姓名']=df_buy['编码'].apply(lambda x: x[:-8])
+        return df_buy
+
+    def read_takens(self,taken_fn):
+        df_taken=pd.read_excel(taken_fn)
+        return df_taken
+
+    def append_buy_to_fn(self,fn='E:\\temp\\minghu\\xlsm\\MH016徐颖丽.xlsm'):
+        #'收款日期','购课编码','购课类型','购课节数','购课时长（天）','应收金额','实收金额','收款人','收入类别','备注'
+        # df_fn=pd.read_excel(fn,sheet_name='购课表')
+        cus_name=fn.split('\\')[-1].split('.')[0]
+        df_had=self.df_buys[self.df_buys['会员编码及姓名']==cus_name]
+        if df_had.shape[0]>0:
+            df_had=df_had.copy()
+            df_had.rename(columns={'编码':'购课编码','购课类别':'收入类别'},inplace=True)
+            df_had.loc[:,'购课节数']=''
+            df_had.loc[:,'购课时长（天）']=''
+            df_input=df_had[['收款日期','购课编码','购课类型','购课节数','购课时长（天）','应收金额','实收金额','收款人','收入类别','备注']]
+
+            append_log=write_data.WriteData().write_to_xlsx(input_dataframe=df_input,output_xlsx=fn,sheet_name='购课表',parse_date_col_name='收款日期')
+            # print(append_log)
+
+    def append_taken_to_fn(self,fn='E:\\temp\\minghu\\xlsm\\MH016徐颖丽.xlsm'):
+        cus_name=fn.split('\\')[-1].split('.')[0]
+        df_cus_taken=self.df_taken[(self.df_taken['会员姓名']==cus_name) & (self.df_taken['是否\n完成']=='是') ]
+        if df_cus_taken.shape[0]>0:
+            df_input=df_cus_taken.copy()
+            df_input.rename(columns={'工作内容':'课程类型'},inplace=True)
+            # print(df_input)
+            df_input=df_input[['日期','时间','时长\n（小时）','课程类型','教练','备注']]
+            apd_log=write_data.WriteData().write_to_xlsx(input_dataframe=df_input,output_xlsx=fn,sheet_name='上课记录',parse_date_col_name='日期')
+            print(apd_log)
+
+    def batch_append_buy(self,dir):
+        for fn in os.list(dir):
+            print('正在添加 {} 的购课记录'.format(fn))
+            filename=os.path.join(dir,fn)
+            self.append_buy_to_fn(fn=filename)
+        print('完成')
+
+    def batch_append_taken(self,dir):
+        for fn in os.list(dir):
+            print('正在添加 {} 的上课记录'.format(fn))
+            filename=os.path.join(dir,fn)
+            self.append_taken_to_fn(fn=filename)
+        print('完成')
+
+    def batch_transfer_short_date(self,dir):
+        shts={'身体数据':['日期'],'训练情况':['日期'],'购课表':['收款日期'],'上课记录':['日期'],
+                            '限时课程记录':['限时课程起始日','限时课程结束日','限时课程实际结束日']}
+
+        for fn in os.listdir(dir):
+            if re.match(r'MH\d{3}.*.xlsm$',fn):
+                filename=os.path.join(dir,fn)
+                for key in shts.keys():
+                    for itm in shts[key]:
+                        style_log=write_data.WriteData().convert_column_to_date_format(file_path=filename,sheet_name=key,column_name=itm)
+                
 
 if __name__=='__main__':
-    p=XlsxToXlsm()
+    #一、从xlsx生成xlsm至目录
+    # p=XlsxToXlsm()
     # fnlist=p.get_list(input_dir='E:\\temp\\minghu\\01-会员管理\\会员资料')
     # p.new_xlsm(template='E:\\temp\\minghu\\01-会员管理\\模板.xlsm',fn_list=fnlist,out_dir='E:\\temp\\minghu\\xlsm')
-    # p.copy_data(xlsx='E:\\temp\\minghu\\01-会员管理\\会员资料\\MH016徐颖丽.xlsx',xlsm='E:\\temp\\minghu\\xlsm\\MH016徐颖丽.xlsm')
 
-    # df_src=pd.read_excel('E:\\temp\\minghu\\01-会员管理\\会员资料\\MH016徐颖丽.xlsx',sheet_name='基本情况')
-    # res=write_data.WriteData().write_to_xlsx(input_dataframe=df_src,output_xlsx='E:\\temp\\minghu\\xlsm\\MH016徐颖丽.xlsm',
-    #             sheet_name='基本情况',parse_date_col_name='')
-    # k=pd.read_excel('E:\\temp\\minghu\\xlsm\\MH016徐颖丽.xlsm',sheet_name='基本情况')
+    #二、批量将xlsx数据copy到新的xlsm中
+    # p.batch_copy_data(xlsx_dir='E:\\temp\\minghu\\01-会员管理\\会员资料',xlsm_dir='E:\\temp\\minghu\\xlsm')
 
-    # print(k)
-    p.batch_copy_data(xlsx_dir='E:\\temp\\minghu\\01-会员管理\\会员资料',xlsm_dir='E:\\temp\\minghu\\xlsm')
+    #三、复制既往的上课及购课数据
+    q=CopyRecs(buy_fn='E:\\temp\\minghu\\客户业务流水数据.xlsx',taken_fn='E:\\temp\\minghu\\教练工作日志合并.xlsx')
+    q.batch_append_buy(dir='E:\\temp\\minghu\\xlsm')
+    q.batch_append_taken(dir='E:\\temp\\minghu\\xlsm')
+
+    #四、批量将每个文件的日期格式改为矩日期
+    # q.batch_transfer_short_date(dir='E:\\temp\\minghu\\xlsm')
