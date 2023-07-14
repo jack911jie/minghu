5823b1463643f709bf1dde52721d8737e9dbc9d6
jack <jack@elaine.com>
2023-07-14 17:03:12 +0800

    feat 发送到期客户的日程封装

--- WeCom/agenda.py
+++ WeCom/agenda.py
@@ -2,67 +2,112 @@ import os
 import json
 # from urllib import parse, request
 import requests
+from datetime import datetime
 
 class WeCom:
     def __init__(self):
         pass
 
-    def get_access_token(self,corp_id='ww1b8f832af7bf55a1',secret='Ssnu5W15iuGhn-G_lTola2mAaMRg_GNutuvtb79viZE'):
-        url='https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={}&corpsecret={}'.format(corp_id,secret)
+    def get_access_token(self,corp_id='ww1b8f832af7bf55a1',secret='Ssnu5W15iuGhn-G_lTola2mAaMRg_GNutuvtb79viZE',
+                        access_token_fn='d:\\temp\\minghu\\access_token\\access_token.txt'):
+        
         try:
-            appid = "you appid"
-            secret = "you secret"
-            textmod = {"grant_type": "client_credential",
-                "appid": appid,
-                "secret": secret
-            }
-            textmod = parse.urlencode(textmod)
-            header_dict = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko'}
-            url = 'https://api.weixin.qq.com/cgi-bin/token'
-            req = request.Request(url='%s%s%s' % (url, '?', textmod), headers=header_dict)
-            res = request.urlopen(req)
-            res = res.read().decode(encoding='utf-8')
-            res = json.loads(res)
-            access_token = res["access_token"]
-            print('access_token:', access_token)
-            return access_token
-        except Exception as e:
-            print(e)
-            return False
+            with open(os.path.join(access_token_fn),'r',encoding='utf-8') as f:
+                lines=f.readlines()
+            save_datetime=datetime.strptime(lines[0].strip(),'%Y-%m-%d %H:%M:%S')     
+            timeinterval=datetime.now()-save_datetime
 
-    def creat_agenda(self):
-        access_token='UZzRFwA3hoP2YClhsv9J7t43fZqhLqtt__0El8UnVgZ5k2j8z0qiFiU-tGhd3vSvHfGgRYz_gPZtVAtzXwoJjIZJwQsRg5fIUUWFR6jDyUu3nvsjgpVgwcq9gBbvWiyE6SvxZ_Jp1G6N1PPfOvtD5AehNWUZSi6-koPGDsPij1u5ZczEiQAhbR5JjFVXJnWoTo0bsgYHdKBGgqRWzJUTUA'
-        url='https://qyapi.weixin.qq.com/cgi-bin/oa/schedule/add?access_token='+access_token
+            #超过2小时的则重新获取access_token
+            if timeinterval.seconds>7200:
+                url=f'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corp_id}&corpsecret={secret}'
+                response = requests.get(url)
+            
+                if response.status_code == 200:
+                    result = response.json()
+                    access_token = result.get("access_token")
+                    if access_token:
+                        txt=datetime.now().strftime('%Y-%m-%d %H:%M:%S')+'\n'+access_token
+                        with open(os.path.join(access_token_fn),'w',encoding='utf-8') as f:
+                            f.write(txt)
+                        return access_token
+                    else:
+                        print("获取 Access Token 失败：" + result.get("errmsg", ""))
+                else:
+                    print("请求失败")
+                
+                return None
+            #2小时以内的从文本读取access_token
+            else:
+                return lines[1].strip()
+        except Exception as err:
+            print('读取本地缓存access_token错误:',err)       
+       
 
-        data={
-                "schedule": {                    
-                    "start_time": 1689379200,
-                    "end_time": 1689379200,                  
-                    "summary": "测试铭湖日程",
-                    "description": "2.0版本需求初步评审",
-                    "attendees": [{
-                            "userid": "AXiao"
-                        }],
-                    "reminders": {
-                        "is_remind": 1,
-                        "remind_before_event_secs": 3600,
-                        "is_repeat": 0,
-                        "repeat_type": 7,
-                        "repeat_until": 1689379200,
-                        "is_custom_repeat": 0,
-                        "repeat_interval": 1,
-                        "repeat_day_of_week": [3, 7],
-                        "repeat_day_of_month": [10, 21],
-                        "timezone": 8
-                    },
-                    "location": "南宁市民族大道"
+    def create_schedule(self,userids, desp, start_time, end_time,corp_id='ww1b8f832af7bf55a1',secret='Ssnu5W15iuGhn-G_lTola2mAaMRg_GNutuvtb79viZE',
+                        access_token_fn='d:\\temp\\minghu\\access_token\\access_token.txt'):
+        access_token=self.get_access_token(corp_id=corp_id,secret=secret,access_token_fn=access_token_fn)
+        # url = f"https://qyapi.weixin.qq.com/cgi-bin/oa/schedule/add?access_token={access_token}&debug=1"
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/oa/schedule/add?access_token={access_token}"
+        start_date=self.trans_date(start_time)
+        end_date=self.trans_date(end_time)
+        
+        data = {
+            "schedule": {		
+                "start_time": start_date,
+                "end_time": end_date,
+                "attendees": [{
+                    "userid": userid
+                } for userid in userids],
+                "summary": "今日限时课程到期会员",
+                "description":desp,
+                "reminders": {
+                    "is_remind": 1,
+                    "remind_before_event_secs": 86400,
+                    "timezone": 8
                 }
             }
+        }
+        
+        response = requests.post(url,json=data)
+        
+        if response.status_code == 200:
+            result = response.json()
+            if result["errcode"] == 0:
+                print("日程创建成功")
+            else:
+                print("日程创建失败：" + result["errmsg"])
+        else:
+            print("请求失败")
 
-        res=requests.post(url,data=data)
-        print(res.status_code,res.text)
+    def trans_date(self,date_input='2023-7-16 8:00:00'):
+        # date_input=date_input+' 8:00:00'
+        # date_input=datetime.now()
+        try:
+            dt=datetime.timestamp(datetime.strptime(date_input,'%Y-%m-%d %H:%M:%S'))
+            # print(dt)
+        except Exception as err:
+            dt=datetime.timestamp(date_input)
+            # print(dt)
+        return int(dt)
+            
 
-
+ 
 if __name__=='__main__':
     p=WeCom()
-    p.creat_agenda()
+    start_time='2023-7-23 8:00:00'
+    end_time=datetime.strptime(start_time.split(' ')[0]+' 23:00:00','%Y-%m-%d %H:%M:%S')
+    p.create_schedule(userids=['AXiao'], 
+                      desp='MH101李测试，\nMH102王测试,\nMH333张测试', 
+                      start_time=start_time,
+                      end_time=end_time,
+                      corp_id='ww1b8f832af7bf55a1',
+                      secret='Ssnu5W15iuGhn-G_lTola2mAaMRg_GNutuvtb79viZE',
+                    access_token_fn='d:\\temp\\minghu\\access_token\\access_token.txt')
+
+    # 配置的可信域名为：jack911jie.github.io
+    # 需配置好ip，在https://work.weixin.qq.com/wework_admin/frame#/apps/modApiApp/5629501898608866 页面中配置好企业可信IP，即：将公网IP写入。
+    # 配置好以上两项后，才能使用代码写入日程。
+    # start_time：日程开始时间，格式为：2023-7-16 8:00:00
+    # end_time：日程结束时间，格式同start_time
+    # 默认设置为提前一天提醒，不重复。
+    # access_token_fn为临时缓存文件，如2小时内发起，则从文件读取，否则根据corp_id及secret重新获取access_token并写入该文件。
